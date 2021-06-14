087b2b56c2e5ef6b531c2e730d26145592af8089
Cigan <makuizafailed@gmail.com>
2021-06-14 22:17:31 +0700

    [INIT]: Initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { TestComponent } from 'react-quick-admin-panel';
+
+function App() {
+    return (
+        <div className="App">
+            <TestComponent />
+        </div>
+    );
+}
+
+export default App;
